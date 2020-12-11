f76edf03dedade5ae70fc6df3d53a7525769a39c
kostya <caxnyk2002@gmail.com>
2020-12-11 21:30:37 +0300

    .

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { Route, Switch } from 'react-router-dom';
+
+import { SignIn } from './pages/SignIn';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/auth" component={SignIn} />
+    </Switch>
+  );
+}
+
+export default App;
---
+++ frontend/src/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import './index.css';
+
+import App from './App';
+import { ThemeProvider, CssBaseline } from '@material-ui/core';
+import theme from './theme';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
